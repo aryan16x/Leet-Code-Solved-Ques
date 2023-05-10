97cbaf9ffa9e130478208e2049e01de4f649b50c
Aryan Gadhiya <ary16.work@gmail.com>
2023-05-11 00:46:22 +0530

    Create 442.cpp

---
+++ Day3/442.cpp
@@ -0,0 +1,19 @@
+// Find All Duplicates in an Array
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector <int> result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i=0; i<(nums.size()-1) & nums.size()>1; i++){
+            if (nums[i]==nums[i+1]){
+                result.push_back(nums[i]);
+                i++;
+            }
+        }
+
+        return result;
+    }
+};
