9e764c4902c407615ffc3db62f7ea04b47fe7223
Aryan Gadhiya <ary16.work@gmail.com>
2023-05-15 22:22:32 +0530

    Create 75.cpp

---
+++ Day6/75.cpp
@@ -0,0 +1,17 @@
+// Sort Colors
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        for (int i=n-1; i>0; i--){
+            for (int j=0; j<i; j++){
+                if (nums[j]>nums[j+1]){
+                    int tmp=nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
+        }
+    }
+};
